c8bf605f7dfe76e56b9f092ac1224fbeeef3d2e8
jeffreyfriedman <jeff@eurodollaryen.com>
2017-01-01 18:23:35 -0500

    show damage and destroy blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,20 +2,48 @@
 using System.Collections;
 
 public class Block : MonoBehaviour {
-	public int maxHits;
+	public Sprite[] hitSprites; 
 	private int timesHit;
+	private LevelManager levelManager;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
+		levelManager = GameObject.FindObjectOfType<LevelManager>();
 		timesHit = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update() {
 
 	}
 
-	void OnCollisionEnter2D (Collision2D col) {
+	void OnCollisionEnter2D(Collision2D col) {
+		bool isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+				HandleHits();
+		}
+	}
+
+	void HandleHits() {
 		timesHit++;
+		int maxHits = hitSprites.Length + 1; // infer max hits by number of damage sprites in array
+		if (timesHit >= maxHits) {
+			Destroy(gameObject);
+		} else {
+			LoadSprites();
+		}
+	}
+
+	void LoadSprites() {
+		int spriteIndex = timesHit - 1;
+		if (hitSprites[spriteIndex]) {  // to guard against accidentally omitted sprites
+			this.GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+		} 
+	}
+
+	// TODO Remove this method once we can actually win
+	void SimulateWin() {
+		levelManager.LoadNextLevel();
 	}
 }
+
